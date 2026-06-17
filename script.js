e81a4a41508f5504cf84947e754fb5677840fa6d
Alperen-Isler <alperenislerdev@gmail.com>
2026-06-17 18:29:16 +0200

    Add logic for adding new Games to library

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
-let container = document.querySelector(".container");
-let bookTitle = document.querySelector("h2");
+const container = document.querySelector(".game-container");
+const bookTitle = document.querySelector("h2");
+const addNewGameButton = document.querySelector(".add-game");
+const addNewGameDialog = document.querySelector(".add-game-dialog");
+const closeModalButton = document.querySelector("#close-modal");
+const newGameForm = document.querySelector("#new-game-form");
 
 const myLibrary = [];
 
-function Game(name, developedBy, hoursPlayed, finished){
+function Game(name, gamePlatform, hoursPlayed, finished){
     if(!new.target){
         throw Error("you must use the 'new' operator to call the constructor")
     }
     this.name = name;
-    this.developedBy = developedBy;
+    this.gamePlatform = gamePlatform;
     this.hoursPlayed = hoursPlayed;
     this.id = crypto.randomUUID();
-    if(finished === true){
+    if(finished === "true"){
         this.finished = "finished the Game"
-    } else if (finished === false){
+    } else if (finished === "false"){
         this.finished = "didn't finish the game yet"
     };
 };
 
-function addGametoLibrary(name, developedBy, hoursPlayed, finished){
-    return myLibrary.push(new Game(name, developedBy, hoursPlayed, finished));
+function addGameToLibrary(name, gamePlatform, hoursPlayed, finished){
+    return myLibrary.push(new Game(name, gamePlatform, hoursPlayed, finished));
 };
 
-addGametoLibrary("GTA 6", "Rockstar games", 0, false);
-addGametoLibrary("The Witcher 4", "CD Project Red", 0, false);
+function renderLibrary(){
+    container.innerHTML = "";
+    myLibrary.forEach(game => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        container.appendChild(card);
+        let gameTitle = document.createElement("h2");
+        card.appendChild(gameTitle);
+        gameTitle.textContent = game.name;
+        let gameDevelopers = document.createElement("p");
+        card.appendChild(gameDevelopers);
+        gameDevelopers.textContent = "Platform: " + game.gamePlatform;
+        let gameHoursPlayed = document.createElement("p");
+        card.appendChild(gameHoursPlayed);
+        gameHoursPlayed.textContent = "Hours played: " + game.hoursPlayed;
+        let gameStatus = document.createElement("p");
+        card.appendChild(gameStatus);
+        gameStatus.textContent = "Game status: " + game.finished;
+    });
+}
 
-console.log(myLibrary);
+addNewGameButton.addEventListener("click", function(){
+    addNewGameDialog.showModal();
+});
 
-for (let i = 0; i < myLibrary.length; i++){
-    let card = document.createElement("div");
-    card.classList.add("card");
-    container.appendChild(card);
-    let gameTitle = document.createElement("h2");
-    card.appendChild(gameTitle);
-    gameTitle.textContent = myLibrary[i].name;
-    let gameDevelopers = document.createElement("p");
-    card.appendChild(gameDevelopers);
-    gameDevelopers.textContent = "developed by: " + myLibrary[i].developedBy;
-    let gameHoursPlayed = document.createElement("p");
-    card.appendChild(gameHoursPlayed);
-    gameHoursPlayed.textContent = "Hours played: " + myLibrary[i].hoursPlayed;
-    let gameStatus = document.createElement("p");
-    card.appendChild(gameStatus);
-    gameStatus.textContent = "Game status: " + myLibrary[i].finished;
+closeModalButton.addEventListener("click", function(){
+    addNewGameDialog.close();
+});
+
+newGameForm.onsubmit = function(e){
+    e.preventDefault();
+    const newGameName = document.querySelector("#new-game-name").value;
+    const newGamePlatform = document.querySelector("input[name='game-platform']:checked").value;
+    const newGameHoursPlayed = document.querySelector("#game-time-played").value;
+    const newGameStatus = document.querySelector("input[name='is-finished']:checked").value;
+    addGameToLibrary(newGameName, newGamePlatform, newGameHoursPlayed, newGameStatus);
+    renderLibrary();
+    newGameForm.reset();
+    addNewGameDialog.close();
 };
